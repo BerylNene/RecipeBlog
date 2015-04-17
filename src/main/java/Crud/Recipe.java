a9220dc62252c8767ce10299100a90ca93cf9d84
Beryl Bawo Nene <'nenebawoberyl@gmail.com'>
2015-04-17 18:21:00 -0400

    All crud is working fine now

---
+++ src/main/java/Crud/Recipe.java
@@ -0,0 +1,232 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Crud;
+import credentials.database;
+import static credentials.database.getConnection;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.stream.JsonParser;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+/**
+ *
+ * @author c0641046
+ */
+@Path("/recipe")
+public class Recipe {
+    
+    @GET
+    @Produces("application/json")
+    public String doGet() {
+
+        String result = resultMethod("SELECT * FROM newrecipeentry ");
+        return result;
+
+    }
+
+    /**
+     * doGet Method takes one parameter of type of string pass all newrecipeentry table
+     * data in resultMethod(). store all table data in String result variable
+     *
+     * @param id
+     * @return result
+     */
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public String doGet(@PathParam("id") String id) {
+        String result = resultMethod("SELECT * FROM newrecipeentry where id=?", id);
+        return result;
+
+    }
+
+    /**
+     * doPost Method takes one parameter of type String. Used to Insert the
+     * values into Product table. get the name, description, quantity by using
+     * HashMap call doUpdate Method
+     *
+     * @param strValue
+     */
+    @POST
+    @Consumes("application/json")
+    public void doPost(String strValue) {
+        JsonParser jsonParserObj = Json.createParser(new StringReader(strValue));
+        Map<String, String> map = new HashMap<>();
+        String name = "", value;
+        while (jsonParserObj.hasNext()) {
+            JsonParser.Event event = jsonParserObj.next();
+            switch (event) {
+                case KEY_NAME:
+                    name = jsonParserObj.getString();
+                    break;
+                case VALUE_STRING:
+                    value = jsonParserObj.getString();
+                    map.put(name, value);
+                    break;
+                case VALUE_NUMBER:
+                    value = Integer.toString(jsonParserObj.getInt());
+                    map.put(name, value);
+                    break;
+            }
+
+        }
+        System.out.println(map);
+        String getEmail = map.get("email");
+        String getAuthor = map.get("author");
+        String getCategory = map.get("category");
+        String getrecipeName = map.get("recipeName");
+        String getingredient = map.get("ingredient");
+        String getStep = map.get("step");
+        String getdateCreated = map.get("dateCreated");
+       
+
+        doUpdate("INSERT INTO newrecipeentry (email,author,category,recipeName,ingredient,step,dateCreated) "
+                + "VALUES (?, ?, ?,?,?,?,?)", getEmail, getAuthor, getCategory,getrecipeName,getingredient,getStep,getdateCreated );
+
+    }
+
+    /**
+     * doPut Method takes two parameters of type string Used to Insert the
+     * values into Product table. get the name, description, quantity by using
+     * HashMap
+     *
+     * @param id
+     * @param strValue
+     */
+    @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    public void doPut(@PathParam("id") String id, String strValue) {
+        JsonParser jsonParserObj = Json.createParser(new StringReader(strValue));
+        Map<String, String> map = new HashMap<>();
+        String name = "", value;
+        while (jsonParserObj.hasNext()) {
+            JsonParser.Event event = jsonParserObj.next();
+            switch (event) {
+                case KEY_NAME:
+                    name = jsonParserObj.getString();
+                    break;
+                case VALUE_STRING:
+                    value = jsonParserObj.getString();
+                    map.put(name, value);
+                    break;
+                case VALUE_NUMBER:
+                    value = Integer.toString(jsonParserObj.getInt());
+                    map.put(name, value);
+                    break;
+            }
+
+        }
+        System.out.println(map);
+        
+        
+         String getEmail = map.get("email");
+        String getAuthor = map.get("author");
+        String getCategory = map.get("category");
+        String getrecipeName = map.get("recipeName");
+        String getingredient = map.get("ingredient");
+        String getStep = map.get("step");
+        String getdateCreated = map.get("dateCreated");
+        
+        doUpdate("update newrecipeentry set email = ?, author = ?, category = ?, recipeName = ? , ingredient = ? , step = ?, dateCreated = ? where id = ?",getEmail, getAuthor, getCategory,getrecipeName,getingredient,getStep,getdateCreated, id);
+    }
+
+    /**
+     * doDelete takes one parameter of type String. Used to delete the values
+     * into Product table. get the name, description, quantity by using Simple
+     * Json Library
+     *
+     * @param id
+     * @param strValue
+     */
+    @DELETE
+    @Path("{id}")
+    @Consumes("application/json")
+    public void doDelete(@PathParam("id") String id, String strValue) {
+        doUpdate("DELETE FROM `newrecipeentry` WHERE `id`=?", id);
+    }
+
+    /**
+     * resultMethod accepts two arguments It executes the Query get ProductID,
+     * name, description, quantity. Used JSON object model and provides methods
+     * to add name/value pairs to the object model and to return the resulting
+     * object
+     *
+     * @param query
+     * @param params
+     * @throws SQLException
+     * @return
+     */
+    private String resultMethod(String query, String... params) {
+        String strJson = "";
+        JsonArrayBuilder jsonArrayObj = Json.createArrayBuilder();
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                JsonObject json = Json.createObjectBuilder()
+                        .add("id", rs.getInt("id"))
+                        .add("email", rs.getString("email"))
+                        .add("author", rs.getString("author"))
+                        .add("category", rs.getString("category"))
+                        .add("recipeName", rs.getString("recipeName"))
+                        .add("ingredient", rs.getString("ingredient"))
+                        .add("step", rs.getString("step"))
+                        .add("dateCreated", rs.getString("dateCreated")).build();
+
+                jsonArrayObj.add(json);
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQL Exception Error: " + ex.getMessage());
+        }
+
+        strJson = jsonArrayObj.build().toString();
+        return strJson;
+    }
+
+    /**
+     * doUpdate Method accepts two arguments Update the entries in the table
+     * 'newrecipeentry'
+     *
+     * @param query
+     * @param params
+     * @return numChanges
+     */
+    private int doUpdate(String query, String... params) {
+        int numChanges = 0;
+        try (Connection conn = getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.err.println("SQL EXception in doUpdate Method" + ex.getMessage());
+        }
+        return numChanges;
+    }
+
+}
+
